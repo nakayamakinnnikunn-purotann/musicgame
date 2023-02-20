5b9ebc7db49790da4c1057b3f8b2ca6bdab5bbb0
nakayamakinnnikunn-purotann <115986363+nakayamakinnnikunn-purotann@users.noreply.github.com>
2023-02-20 09:13:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,81 +1,26 @@
-// ノートの初期位置
-const initialNotePosition = -100;
+const notes = [0, 1, 2, 3]; // 4つのノート
 
-// ノーツのスピード
-const noteSpeed = 0.1;
-
-// スコア
-let score = 0;
-
-// キーコード
-const keyCodes = {
-  65: "red", // A
-  83: "yellow", // S
-  68: "blue", // D
-  70: "green" // F
-};
-
-// キーイベントの追加
-document.addEventListener("keydown", event => {
-  if (event.keyCode in keyCodes) {
-    const key = document.querySelector(`.key.${keyCodes[event.keyCode]}`);
-    key.classList.add("active");
-    checkNoteCollision(keyCodes[event.keyCode]);
-  }
-});
-
-document.addEventListener("keyup", event => {
-  if (event.keyCode in keyCodes) {
-    const key = document.querySelector(`.key.${keyCodes[event.keyCode]}`);
-    key.classList.remove("active");
-  }
-});
-
-// ノートの生成
-function createNote() {
-  const colors = Object.values(keyCodes);
-  const randomColor = colors[Math.floor(Math.random() * colors.length)];
-  const note = document.createElement("div");
-  note.classList.add("note", randomColor);
-  note.style.left = Math.floor(Math.random() * 520) + "px";
-  document.querySelector(".notes-container").appendChild(note);
+// ノートを押した時の処理
+function pressNote() {
+  $(this).addClass("note-pressed");
 }
 
-// ノートの衝突判定
-function checkNoteCollision(color) {
-  const note = document.querySelector(`.note.${color}`);
-  if (note) {
-    const notePosition = note.getBoundingClientRect().top;
-    const keyPosition = document.querySelector(`.key.${color}`).getBoundingClientRect().bottom;
-    const noteWidth = note.getBoundingClientRect().width;
-    if (notePosition < keyPosition && notePosition > keyPosition - noteWidth) {
-      note.remove();
-      score++;
-      updateScore();
-    }
-  }
+// ノートを離した時の処理
+function releaseNote() {
+  $(this).removeClass("note-pressed");
 }
 
-// スコアの更新
-function updateScore() {
-  const scoreElement = document.querySelector(".score-container span:last-child");
-  scoreElement.textContent = score;
-}
-
-// メインループ
-function loop() {
-  createNote();
-  const notes = document.querySelectorAll(".note");
-  notes.forEach(note => {
-    const notePosition = note.getBoundingClientRect().top;
-    if (notePosition > 500) {
-      note.remove();
-    } else {
-      note.style.top = notePosition + noteSpeed + "px";
-    }
+// ノートを生成して配置する処理
+function generateNotes() {
+  notes.forEach((note) => {
+    const noteContainer = $(`[data-note="${note}"]`);
+    const noteElem = $("<div>").addClass("note");
+    noteElem.on("mousedown touchstart", pressNote);
+    noteElem.on("mouseup touchend", releaseNote);
+    noteContainer.append(noteElem);
   });
-  requestAnimationFrame(loop);
 }
 
-// ゲーム開始
-loop();
+$(document).ready(() => {
+  generateNotes();
+});
